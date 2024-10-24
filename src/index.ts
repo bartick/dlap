5a86f88a9b1cf288b87a206ad9a9b8899fece3d2
bartick <bartickmaiti1998@gmail.com>
2024-10-24 14:17:16 +0530

    add: better comments

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ import * as http from "http";``
 
 import { TOKEN } from './utils/config';
 import { BaseCommand } from './utils/BaseCommand';
-// import { BaseModal } from './utils/BaseModal';
 import app from './rest';
 
+// Create a new client instance for the discord bot
 const client = new Client({
     intents: [
         'Guilds',
@@ -25,13 +25,15 @@ const client = new Client({
     }
 });
 
+// Listen for the ready event and log when the bot is ready
 client.once(Events.ClientReady, (client) => {
     console.log(`Logged in as ${client.user?.tag}`);
 });
 
+// Create an array to store all the commands
 const commands: BaseCommand[] = [];
-// const modals: BaseModal[] = [];
 
+// Listen for the interactionCreate event and handle the commands
 client.on(Events.InteractionCreate, async (interaction: Interaction) => {
     if (interaction.isCommand()) {
         const command = commands.find(cmd => cmd.name === interaction.commandName);
@@ -43,26 +45,21 @@ client.on(Events.InteractionCreate, async (interaction: Interaction) => {
             console.error(error);
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
-    } 
-    // else if (interaction.isModalSubmit()) {
-    //     const modal = modals.find(modal => modal.customId === interaction.customId);
-    //     if (!modal) return;
-
-    //     try {
-    //         await modal.run(interaction);
-    //     } catch (error) {
-    //         console.error(error);
-    //         await interaction.reply({ content: 'There was an error while executing this modal!', ephemeral: true });
-    //     }
-    // }
-
+    }
     
 })
 
+// Create a server to listen for incoming rest requests
 let server: http.Server;
+
+
 console.log('Logging in...');
+
+// Load all the commands and then start the required services
 (async () => {
+    // Get all commands from the commands folder
     const commandFiles = fs.readdirSync(path.join(__dirname, "./commands")).filter(file => file.endsWith(".ts"));
+    // Import all the commands and add them to the commands array
     for (const file of commandFiles) {
         const command = await import(`./commands/${file}`);
         if (command.default?.prototype instanceof BaseCommand) {
@@ -70,20 +67,14 @@ console.log('Logging in...');
         }
     }
 
-    // const modalFiles = fs.readdirSync(path.join(__dirname, "./modals")).filter(file => file.endsWith(".ts"));
-    // for (const file of modalFiles) {
-    //     const modal = await import(`./modals/${file}`);
-    //     if (modal.default?.prototype instanceof BaseModal) {
-    //         if (modals.find(_modal => _modal.customId === (modal.default?.prototype.customId)))
-    //             throw new Error(`Duplicate customId found in ${file}`);
-    //         modals.push(new modal.default());
-    //     }
-    // }
-
+    // Connect to the database
     // await dbconnect();
 
 })().then(() => {
+    // Start the bot
     client.login(TOKEN);
+
+    // Start the rest server
     server = app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
@@ -91,8 +82,8 @@ console.log('Logging in...');
 
 process.on('SIGINT', async () => {
     console.log('Shutting down...');
-    // await dbclose();
-    await client.destroy();
-    if(server) server.close();
-    process.exit(0);
+    // await dbclose(); // Close the database connection
+    await client.destroy(); // Destroy the discord client
+    if(server) server.close(); // Close the rest server
+    process.exit(0); // Exit the process
 });
--- src/register.ts
+++ src/register.ts
@@ -3,21 +3,23 @@ import { BOTID, TOKEN } from "./utils/config";
 import fs from "fs";
 import path from "path";
 
-
+// Register all the commands with the discord api
 async function register() {
-    const commands = [];
-    const commandFiles = fs.readdirSync(path.join(__dirname, "./commands")).filter(file => file.endsWith(".ts"));
+    const commands = []; // Create an array to store all the commands
+    const commandFiles = fs.readdirSync(path.join(__dirname, "./commands")).filter(file => file.endsWith(".ts")); // Get all commands from the commands folder
 
+    // Loop through all the commands and add them to the commands array
     for (const file of commandFiles) {
         const command = await import(`./commands/${file}`);
         commands.push(new command.default().build().toJSON());
     }
 
-    const rest = new REST({ version: "10" }).setToken(TOKEN);
+    const rest = new REST({ version: "10" }).setToken(TOKEN); // Create a new rest client using version 10 of the discord api
 
     try {
         console.log("Started refreshing application (/) commands.");
 
+        // Register all the commands with the discord api
         const data = await rest.put(
 			Routes.applicationCommands(BOTID),
 			{ body: commands },
