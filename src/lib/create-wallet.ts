809208a5a88dda5b7bf4ea1e167f1b519ca6c9bc
bartick <bartickmaiti1998@gmail.com>
2024-10-24 12:05:33 +0530

    add: comments

--- src/lib/create-wallet.ts
+++ src/lib/create-wallet.ts
@@ -1,6 +1,10 @@
 import { Keypair } from '@solana/web3.js';
 
-// Generate a new wallet
+/**
+ * Generate a new wallet with a new keypair (public and secret key)
+ * 
+ * @returns publicKey and secretKey as strings
+ */
 export function createWallet() {
     // Generate a new keypair
     const keypair = Keypair.generate();
