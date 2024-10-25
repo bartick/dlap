812dd5d05c039abb417e63d358b4c5a3580367f6
bartick <bartickmaiti1998@gmail.com>
2024-10-25 12:49:14 +0530

    add: better comments

--- src/utils/BaseCommand.ts
+++ src/utils/BaseCommand.ts
@@ -5,18 +5,30 @@ import {
 } from 'discord.js';
 import type {
     ChatInputCommandInteraction,
-    Interaction,
     SlashCommandOptionsOnlyBuilder,
     SlashCommandSubcommandsOnlyBuilder,
 } from 'discord.js';
 
+// Create a base class to create a slash command
 export abstract class BaseCommand {
-    public name: string;
+    public name: string; // The name of the command
 
     constructor(options: { name: string }) {
         this.name = options.name;
     }
 
+    /**
+     * Create an abstract method to build the command
+     * 
+     * @returns SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder
+     */
     abstract build(): SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | SlashCommandOptionsOnlyBuilder;
+
+    /**
+     * Create an abstract method to run the command
+     * 
+     * @param interaction ChatInputCommandInteraction
+     * @returns Promise<void>
+     */
     abstract run(interaction: ChatInputCommandInteraction): Promise<void>;
 }
