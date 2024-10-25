cc2fae275aa2301c6b237a1e60311495dc655bfc
bartick <bartickmaiti1998@gmail.com>
2024-10-26 01:39:56 +0530

    fix: types

--- src/models/secret.model.ts
+++ src/models/secret.model.ts
@@ -1,16 +1,19 @@
-import { Model, DataTypes, BuildOptions, Sequelize } from 'sequelize';
+import { Model, DataTypes, BuildOptions, Sequelize, InferAttributes } from 'sequelize';
 
-export interface SecretModel extends Model {
+// Define the secret model
+export interface SecretModel extends Model<InferAttributes<SecretModel>, InferAttributes<SecretModel>> {
     id: number;
     secret: string;
     createdAt: Date;
     updatedAt: Date;
 }
 
-export type SecretStatic = typeof Model & {
+// Define the secret static model
+export type SecretStatic = typeof Model<InferAttributes<SecretModel>, InferAttributes<SecretModel>> & {
     new (values?: object, options?: BuildOptions): SecretModel;
 };
 
+// Define the secret factory
 export default function SecretFactory(sequelize: Sequelize): SecretStatic {
     return <SecretStatic>sequelize.define('Secret', {
         id: {
