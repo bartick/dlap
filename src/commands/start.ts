53fe403fedea683fcdf246da85069b8e18612b89
bartick <bartickmaiti1998@gmail.com>
2024-10-26 01:32:54 +0530

    fix: now provides a button to start authorization

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,5 +1,6 @@
 import { BaseCommand } from '../utils/BaseCommand';
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { CLIENT_ID, REDIRECT_URL } from '../utils/config';
 
 export default class Start extends BaseCommand {
     constructor() {
@@ -15,7 +16,25 @@ export default class Start extends BaseCommand {
     }
 
     async run(interaction: ChatInputCommandInteraction) {
-        await interaction.reply({ content: 'Starting bot...' });
-        await interaction.editReply('Bot started!');
+        const url = 'https://discord.com/api/oauth2/authorize?' + new URLSearchParams({
+            client_id: CLIENT_ID, // Add your client ID
+            response_type: 'code', 
+            redirect_uri: REDIRECT_URL, // Add your redirect URL
+        }).toString() + '&scope=identify+email'; // Add the scopes you need
+        
+        // Send the user the link to authorize the bot with the required scopes
+        await interaction.reply({
+            content: 'Now starting you registered for the bot',
+            components: [
+                new ActionRowBuilder<ButtonBuilder>()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel('Authorize')
+                            .setURL(url)
+                            .setStyle(ButtonStyle.Link)
+                    )
+            ],
+            ephemeral: true
+        });
     }
 }
